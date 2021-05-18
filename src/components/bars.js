6e657fcdc8d8bbca0f948790907e665deb7a964e
Juan Andrés Caycedo S <ja.caycedo@uniandes.edu.co>
2021-05-18 14:42:13 -0500

    ASd

--- src/components/bars.js
+++ src/components/bars.js
@@ -6,59 +6,65 @@ const Bar = () => {
 
   useEffect(() => {
     const canvas = d3.select(barras.current);
-    d3.json("https://gist.githubusercontent.com/josejbocanegra/000e838b77c6ec8e5d5792229c1cdbd0/raw/83cd9161e28e308ef8c5363e217bad2b6166f21a/countries.json")
-    .then(dataS => {
-        console.log(dataS);
-        
-        const data = dataS;
-        
-        const width = 700;
-        const height = 500;
-        const margin = { top: 10, left: 50, bottom: 40, right: 10 };
-        const iwidth = width - margin.left - margin.right;
-        const iheight = height - margin.top - margin.bottom;
-        
-        const svg = canvas.append("svg");
-        svg.attr("width", width);
-        svg.attr("height", height);
-        
-        let g = svg
+    d3.json(
+      "https://gist.githubusercontent.com/josejbocanegra/d3b9e9775ec3a646603f49bc8d3fb90f/raw/3a39300c2a2ff8644a52e22228e900251ec5880a/population.json"
+    ).then((dataS) => {
+      console.log(dataS);
+
+      const data = [
+        { name: "Juan", age: 3 },
+        { name: "Orlando", age: 39 },
+        { name: "María", age: 7 },
+        { name: "Sandra", age: 35 },
+        { name: "Fernanda", age: 16 },
+        { name: "Maribel", age: 45 },
+        { name: "Sofía", age: 6 },
+      ];
+
+      const width = 700;
+      const height = 500;
+      const margin = { top: 10, left: 50, bottom: 40, right: 10 };
+      const iwidth = width - margin.left - margin.right;
+      const iheight = height - margin.top - margin.bottom;
+
+      const svg = canvas.append("svg");
+      svg.attr("width", width);
+      svg.attr("height", height);
+
+      let g = svg
         .append("g")
         .attr("transform", `translate(${margin.left},${margin.top})`);
-        
-        const y = d3.scaleLinear().domain([0, 30]).range([iheight, 0]);
-        
-        const x = d3
+        let valu = dataS.map((e)=>{return parseInt(e.value)} )
+        const y = d3.scaleLinear().domain([0, 1000000]).range([iheight, 0]);
+
+      const x = d3
         .scaleBand()
-        .domain(data.map((d) => d.country))
+        .domain(dataS.map((d) => d.name))
         .range([0, iwidth])
         .padding(0.1);
-        
-        const bars = g.selectAll("rect").data(data);
-        
-        bars
+
+      const bars = g.selectAll("rect").data(dataS);
+
+      bars
         .enter()
         .append("rect")
         .attr("class", "bar")
         .style("fill", "steelblue")
-        .attr("x", (d) => x(d.country))
-        .attr("y", (d) => y(d.country))
-        .attr("height", (d) => iheight - y(d.population))
+        .attr("x", (d) => x(d.name))
+        .attr("y", (d) => y(d.value))
+        .attr("height", (d) => iheight - y(d.value))
         .attr("width", x.bandwidth());
-        
-        g.append("g")
+
+      g.append("g")
         .classed("x--axis", true)
         .call(d3.axisBottom(x))
         .attr("transform", `translate(0, ${iheight})`);
-        
-        g.append("g").classed("y--axis", true).call(d3.axisLeft(y));
+
+      g.append("g").classed("y--axis", true).call(d3.axisLeft(y));
     });
-});
-    
-    return (
-        <svg ref={barras}></svg>
-        );
-    };
-    
-    export default Bar;
-    
+  });
+
+  return <svg height="500" width ="700" ref={barras}></svg>;
+};
+
+export default Bar;
